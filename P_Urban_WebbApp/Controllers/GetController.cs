c970e8e02cb10ede8b7a47b6b47e97369437f23f
Paweł Urban <urbanpawel@o2.pl>
2018-06-09 14:39:48 +0200

    Pkt I and II done. Method of Index GetControllera added

---
+++ P_Urban_WebbApp/Controllers/GetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P_Urban_WebbApp.Controllers
+{
+    public class GetController : Controller
+    {
+
+        public IActionResult Index(String name)
+        {
+
+            // ViewData["name"] = HtmlEncoder.Default.Encode("Tu metoda Index GetControllera" + name);
+            ViewData["name"] = name;
+            return View();
+        }
+    }
+}
---
+++ P_Urban_WebbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P_Urban_WebbApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
